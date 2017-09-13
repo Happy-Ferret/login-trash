e73b87b348e122a25195665c30d27277fbfb9d91
Happy-Ferret <MarkBauer@gmx.ch>
2017-09-13 19:55:20 +0200

    Updated clock
    Changed time format to 12 hour clock.
    
    Later on we should have a pref (24hrTime.enabled)
    and choose the right format based on this setting.

--- js/clock.js
+++ js/clock.js
@@ -12,6 +12,18 @@ this.clock = doc.getElementById('clock');
     var date = new Date(),
     hours = date.getHours() + '', // get hours as string
     minutes = date.getMinutes() + ''; // get minutes as string
+    seconds = date.getSeconds() + ''; // get seconds as string
+    daytime = "AM"
+
+    // 12 hour clock (make optional -- but set default -- later)
+    if (hours >= 12) {
+      hours = hours - 12;
+      daytime = "PM";
+      }
+
+    if (hours == 0) {
+      hours = 12; 
+    }
 
     // pad with zero if needed
     if (hours.length < 2)
@@ -19,5 +31,5 @@ this.clock = doc.getElementById('clock');
     if (minutes.length < 2)
       minutes = '0' + minutes;
 
-    clock.textContent = hours + ':' + minutes;
+    clock.textContent = hours + ':' + minutes + ':' + seconds + ' ' + daytime;
   }
